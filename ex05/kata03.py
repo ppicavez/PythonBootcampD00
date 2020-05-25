390d6a836c5e6821e473afce360ba67359e993ba
ppicavez <ppicavez@student.42lyon.fr>
2020-05-25 20:15:16 +0200

    ex05 bug fixed on kata03

--- ex05/kata03.py
+++ ex05/kata03.py
@@ -2,4 +2,4 @@
 phrase = "The right format"
 # Exemple : https://pyformat.info/#string_pad_align
 # align right with  character ‘-’ padding 42 characters
-print(f"{phrase:->42}")
+print(f"{phrase:->42}",end='')
