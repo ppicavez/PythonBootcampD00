2dad01428e3bfea58c3b8a9f7010f1fbba10dc6c
ppicavez <ppicavez@student.42lyon.fr>
2020-05-25 21:46:07 +0200

    ex06 PEP bug fixed

--- ex06/recipe.py
+++ ex06/recipe.py
@@ -36,13 +36,13 @@ def print_recipe(recipe_name: str):
 
 
 def print_cookbook(cook_book: dict):
-        for recipe in cook_book:
-            print_recipe(recipe)
+    for recipe in cook_book:
+        print_recipe(recipe)
 
 
 def delete_recipe(recipe_name: str):
-    print(f" Are you sure to delete recipe : \"{recipe_name}\" (YES to delete).")
-    security = input(" : ") 
+    print(f" Are you sure to delete  : \"{recipe_name}\" (YES to delete).")
+    security = input(" : ")
     if security == "YES":
         try:
             del cookbook[recipe_name]
@@ -52,7 +52,6 @@ def delete_recipe(recipe_name: str):
 
 
 def add_recipe(recipe_name: str, ingredients: list, meal: str, prep_time: int):
-    
     cookbook[recipe_name] = {
         "ingredients": ingredients,
         "meal": meal,
@@ -62,7 +61,6 @@ def add_recipe(recipe_name: str, ingredients: list, meal: str, prep_time: int):
 
 
 def principal():
-    
     menu = (
         "Please select an option by typing the corresponding number:\n"
         "1: Add a recipe\n"
@@ -79,7 +77,7 @@ def principal():
     while True:
         print(menu)
         answer = input()
-        while not (answer >'0' and answer < '6'):
+        while not (answer > '0' and answer < '6'):
             os.system("clear")
             print(menu)
             print(wrong_input)
@@ -87,28 +85,33 @@ def principal():
         # choosing menu options
         if answer == "1":
             new_name = input("Recipe name:\n")
-            new_ingredients = input("Ingredients list (separated by , )\n").split(",")
+            new_ing = input("Ingredients list (separated by , )\n").split(",")
             new_meal = input("Meal type:\n")
             time_ok = False
             while not time_ok:
-                    str_new_time = input("Preparation time (in minutes):\n")
-                    if str_new_time.isdigit():
-                        time_ok = True
-                        new_time = int(str_new_time)
-                    else:
-                        print("Not a number. Try again\n")
-            add_recipe(new_name, new_ingredients, new_meal, new_time)
+                str_new_time = input("Preparation time (in minutes):\n")
+                if str_new_time.isdigit():
+                    time_ok = True
+                    new_time = int(str_new_time)
+                else:
+                    print("Not a number. Try again\n")
+            add_recipe(new_name, new_ing, new_meal, new_time)
         elif answer == "2":
             recipe_name = input("Recipe's name to delete?\n")
             delete_recipe(recipe_name)
         elif answer == "3":
-            recipe_name = input("Please enter the recipe's name to get its details:?\n")
-            print_recipe(recipe_name)
+            name = input("Please enter the recipe's name\
+                to get its details:?\n")
+            try:
+                print_recipe(name)
+            except KeyError:
+                print(f"Recipe \"{name}\" not found ")
         elif answer == "4":
             print_cookbook(cookbook)
         elif answer == "5":
             print("Cookbook closed.")
             break
+
 
 if __name__ == "__main__":
     principal()
